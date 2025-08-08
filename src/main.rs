d1bb04f7013acda6161a0967324aa9e264eeca7c
jjwc3 <155734556+jjwc3@users.noreply.github.com>
2025-08-09 00:20:44 +0900

    250809

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,30 @@ fn read_line() -> io::Result<String> {
     io::stdin().lock().read_line(&mut buffer)?;
     Ok(buffer.trim().to_string())
 }
-fn main() {
-    let input = read_line().unwrap().chars().collect::<Vec<_>>();
 
-    for i in 'a'..='y' {
-        let res = input.iter().position(|&s| s == i);
-        match res {
-            Some(c) => print!("{} ", c),
-            None => print!("-1 ")
+fn main() {
+    let input = read_line().unwrap();
+    let char_vec = input.chars().map(|s| s.to_ascii_lowercase()).collect::<Vec<_>>();
+    let mut kind = char_vec.clone();
+    kind.sort();
+    kind.dedup();
+
+    let mut t1 = ' ';
+    let mut c1 = 0;
+    let mut c2 = 0;
+    for s in kind {
+        let count = char_vec.iter().filter(|&t| *t==s).count();
+        if count > c1 {
+            c1 = count;
+            t1 = s;
+        } else if count == c1 {
+            c2 = count;
         }
     }
-    let z = input.iter().position(|&s| s== 'z');
-    match z {
-        Some(c) => print!("{}", c),
-        None => print!("-1")
-    }
 
+    if c1 == c2 {
+        println!("?")
+    } else {
+        println!("{}", t1.to_ascii_uppercase())
+    }
 }
