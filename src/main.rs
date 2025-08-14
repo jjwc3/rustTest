d7745d11119d21c29fafdc912fdaa40409f37631
jjwc3 <jjwjjw2007@naver.com>
2025-08-14 15:28:53 +0900

    250814

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::io::{BufRead, self};
-use std::collections::HashMap;
 
 fn read_line() -> io::Result<String> {
     let mut buffer = String::new();
@@ -8,24 +7,15 @@ fn read_line() -> io::Result<String> {
 }
 
 fn main() {
-    let dict: HashMap<&str, f32> = [("A+", 4.5), ("A0", 4.0), ("B+", 3.5), ("B0", 3.0), ("C+", 2.5), ("C0", 2.0), ("D+", 1.5), ("D0", 1.0), ("F", 0.0)]
-        .iter().cloned().collect();
-    let mut divisor = 0.0; // 학점 * 과목평점(dict)의 합
-    let mut dividend = 0.0; // 학점의 합
+    let input = read_line().unwrap().parse::<u32>().unwrap();
 
-    for _i in 0..20 {
-        let input =  read_line().unwrap();
-        let input_vec = input.split_whitespace().collect::<Vec<_>>();
-
-        if input_vec[2] == "P" {
-            continue
-        } else {
-            let unit = input_vec[1].parse::<f32>().unwrap();
-            let subject_point = *dict.get(input_vec[2]).unwrap();
-            divisor += unit * subject_point;
-            dividend += unit;
+    for i in 1..input {
+        let split_digit = i.to_string().chars().map(|s| s.to_digit(10).unwrap()).collect::<Vec<_>>();
+        let res = i + split_digit.iter().sum::<u32>();
+        if input == res {
+            println!("{}", i);
+            return;
         }
     }
-
-    println!("{}", divisor/dividend);
+    println!("0")
 }
