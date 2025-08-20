7fdac480986195b5b596c3659b925526573f9a22
jjwc3 <jjwjjw2007@naver.com>
2025-08-20 15:24:38 +0900

    250820

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,70 @@
-use std::io::{BufRead, self};
-
-fn read_line() -> io::Result<String> {
-    let mut buffer = String::new();
-    io::stdin().lock().read_line(&mut buffer)?;
-    Ok(buffer.trim().to_string())
-}
+use std::io::{BufRead, self, BufWriter, Write};
+use std::collections::VecDeque;
 
 fn main() {
-    let input = read_line().unwrap().parse::<u32>().unwrap();
+    let stdin = io::stdin();
+    let mut reader = stdin.lock();
+    let mut buffer = String::new();
 
-    for i in 1..input {
-        let split_digit = i.to_string().chars().map(|s| s.to_digit(10).unwrap()).collect::<Vec<_>>();
-        let res = i + split_digit.iter().sum::<u32>();
-        if input == res {
-            println!("{}", i);
-            return;
+    let stdout = io::stdout();
+    let mut writer = BufWriter::new(stdout.lock());
+
+    reader.read_line(&mut buffer).unwrap();
+    let input1: usize = buffer.trim().parse().unwrap();
+
+    let mut deque = VecDeque::new();
+
+    for _ in 0..input1 {
+        buffer.clear();
+        reader.read_line(&mut buffer).unwrap();
+        let input2: Vec<usize> = buffer.split_whitespace().map(|s| s.parse().unwrap()).collect();
+
+        match input2[0] {
+            1 => {
+                deque.push_front(input2[1])
+            },
+            2 => {
+                deque.push_back(input2[1])
+            },
+            3 => {
+                if let Some(num) = deque.pop_front() {
+                    writeln!(writer, "{}", num).unwrap();
+                } else {
+                    writeln!(writer, "-1").unwrap();
+                }
+            },
+            4 => {
+                if let Some(num) = deque.pop_back() {
+                    writeln!(writer, "{}", num).unwrap();
+                } else {
+                    writeln!(writer, "-1").unwrap();
+                }
+            },
+            5 => {
+                writeln!(writer, "{}", deque.len()).unwrap()
+            },
+            6 => {
+                if deque.is_empty() {
+                    writeln!(writer, "1").unwrap();
+                } else {
+                    writeln!(writer, "0").unwrap();
+                }
+            },
+            7 => {
+                if let Some(num) = deque.front() {
+                    writeln!(writer, "{}", num).unwrap();
+                } else {
+                    writeln!(writer, "-1").unwrap();
+                }
+            },
+            8 => {
+                if let Some(num) = deque.back() {
+                    writeln!(writer, "{}", num).unwrap();
+                } else {
+                    writeln!(writer, "-1").unwrap();
+                }
+            },
+            _ => {}
         }
     }
-    println!("0")
 }
